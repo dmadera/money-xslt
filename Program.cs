03fb841f09c163f5103b8a55f81577391ccdea93
Daniel Maděra <madera.dan@gmail.com>
2021-06-11 16:30:41 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using System.Text;
+using System.IO;
+
+namespace money_xslt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = args[0];
+            string template = args[1];
+            string output = args[2];
+
+            XslTransform xslt = new XslTransform();
+            xslt.Load(template);
+            XPathDocument xpathdocument = new XPathDocument(input);
+            using(XmlTextWriter writer = new XmlTextWriter(output, Encoding.UTF8)) {
+                xslt.Transform(xpathdocument, null, writer, null);
+            }
+            string[] lines = File.ReadAllLines(output);
+
+            for (int i = Math.Max(0, lines.Length - 10); i < lines.Length; ++i) {
+                Console.WriteLine(lines[i]);
+            }
+        }
+    }
+}
